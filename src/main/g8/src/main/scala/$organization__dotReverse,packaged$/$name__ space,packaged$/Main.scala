99b8e385ac72b41f99688bb5bf980bf2a06fb585
Martin Pallmann <hi@martinpallmann.de>
2022-05-26 19:01:09 +0200

    Test

--- src/main/g8/src/main/scala/$organization__dotReverse,packaged$/$name__ space,packaged$/Main.scala
+++ src/main/g8/src/main/scala/$organization__dotReverse,packaged$/$name__ space,packaged$/Main.scala
@@ -1,4 +1,9 @@
 package $organization;format="dotReverse,package"$.$name;format="package"$
 
+// Organization dotReverse $organization;format="dotReverse"
+// Organization space $organization;format="space"
+// Organization packaged $organization;format="packaged"
+// Organization dotReverse,packaged $organization;format="dotReverse,packaged"
+
 object Main:
   def main(args: Array[String]): Unit = ???
