8435728dd4753968103ac3b1e20fdb3aca9509f4
Martin Pallmann <hi@martinpallmann.de>
2022-05-26 19:44:32 +0200

    Update Test.scala

--- src/main/g8/src/test/scala/$package$/$name__space,package,packaged$/Test.scala
+++ src/main/g8/src/test/scala/$package$/$name__space,package,packaged$/Test.scala
@@ -1,5 +1,11 @@
 package $organization;format="dotReverse,package"$.$name;format="space,package"$
 
-class Test:
-  def test("test"): Unit = ???
+import munit.FunSuite
+
+class Test extends FunSuite:
+  test("a is smaller than b") {
+    val a = 1
+    val b = 2
+    assert(a > b, "a was smaller than b")
+  }
 
